ad04f92396c166c665bf6590cb534bf5221193f3
Seymurss <141709929+Seymurss@users.noreply.github.com>
2025-07-30 22:50:38 +0400

    login method

--- app/Http/Controllers/AdminAuthController.php
+++ app/Http/Controllers/AdminAuthController.php
@@ -8,19 +8,25 @@ use Illuminate\Support\Facades\Auth;
 
 class AdminAuthController extends Controller
 {
-    public function login(Request $request)
-    {
-        $credentials = $request->only(['email', 'password']);
-
+  public function login(Request $request)
+{
+    \Log::info('Login metodu çağrıldı');
+    $credentials = $request->only(['email', 'password']);
+    try {
         if (!$token = Auth::guard('api')->attempt($credentials)) {
+            \Log::warning('Kimlik doğrulama başarısız');
             return response()->json(['message' => 'Bilgiler yanlış'], 401);
         }
-
         return response()->json([
             'token' => $token,
             'admin' => Auth::guard('api')->user()
         ]);
+    } catch (\Exception $e) {
+        \Log::error('Login sırasında hata: '.$e->getMessage());
+        return response()->json(['error' => 'Sunucu hatası'], 500);
     }
+}
+
 
     public function me()
     {
